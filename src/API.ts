1ca3a25cd7a2eb8fd0f558f5e5d274b6bbe64e23
Renato Delboni <renatonato_jr@hotmail.com>
2022-03-17 11:25:52 +0900

    fetching answer an putting inside array
    I will be easy to pick it out later when mapping thru the answers

--- src/API.ts
+++ src/API.ts
@@ -1,16 +1,34 @@
+import { shuffleArray }from './utils';
+
 export enum Difficulty {
   EASY = "easy",
   MEDIUM = "medium",
   HARD = "hard"
 }
 
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: Difficulty;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+export type QuestionState = Question & { answers: string[] };
 
 export const fetchData = async (amount: number, difficulty: Difficulty) => {
-  const url =`https://opentdb.com/api.php?amount=`;
+  const url = `https://opentdb.com/api.php?amount=`;
   const response = await fetch(
     `${url}${amount}&difficulty=${difficulty}&type=multiple`
   );
   const data = await response.json();
   console.log(data);
-  return data;
-}
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([
+      ...question.incorrect_answers,
+      question.correct_answer
+    ])
+  }));
+};
