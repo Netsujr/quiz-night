8bf51fa41abd201a457686acb9d7c97d0e43b7b2
Renato Delboni <renatonato_jr@hotmail.com>
2022-03-18 12:09:01 +0900

    minor refactor of types

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
+//types
+import  { AnswerObject } from '../App';
 
 type Props = {
   question: string;
   answers: string[];
   callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
-  userAnswer: any;
+  userAnswer: AnswerObject | undefined;
   questionNumber: number;
   totalQuestions: number;
 }
@@ -24,7 +26,7 @@ const QuestionCard: React.FC<Props> = ({
     <div>
       { answers.map(answer => (
         <div key={ answer }>
-          <button disabled={ userAnswer } value={ answer } onClick={ callback }>
+          <button disabled={ !!userAnswer } value={ answer } onClick={ callback }>
             <span dangerouslySetInnerHTML={ { __html: answer } }></span>
           </button>
         </div>
